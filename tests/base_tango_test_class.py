b16ecef7e5e092c704c35c421bc83b049798ed6a
Theo Lemay <theol0403@gmail.com>
2024-12-11 12:16:16 -0400

    CIP-3172 feat: deployment tests check all statuses

--- tests/base_tango_test_class.py
+++ tests/base_tango_test_class.py
@@ -7,14 +7,34 @@ from ska_tango_testing.integration import TangoEventTracer
 
 class BaseTangoTestClass:
 
+    @pytest.fixture(scope="session") 
+    def all_proxies(self, logger: Logger):
+        """Cache all device proxies"""
+        proxies_cache = {}
+
+        # Pre-load all combinations for indices 1-6
+        logger.info("Creating proxies for all devices...")
+        for idx in range(1, 7):
+            cache_key = (idx,)
+            proxies_cache[cache_key] = {
+                k: {idx: create_proxy(idx, k)} for k in DeviceKey
+            }
+        
+        logger.info("Proxies created.")
+        return proxies_cache
+
+
     @pytest.fixture()
-    def initialize_with_indices(self, request, logger: Logger, emulator_base_url: str) -> None:
+    def initialize_with_indices(self, request, logger: Logger, emulator_base_url: str, all_proxies) -> None:
         idxs = request.param
         self.loaded_idxs = idxs if isinstance(idxs, list) else [idxs]
-
         self.logger = logger
+
         self.fqdns = {k: {i: get_fqdn(i, k) for i in self.loaded_idxs} for k in DeviceKey}
-        self.proxies = {k: {i: create_proxy(i, k) for i in self.loaded_idxs} for k in DeviceKey}
+
+        cache_key = tuple(sorted(self.loaded_idxs))
+        self.proxies = all_proxies[cache_key]
+
         self.emulator_urls = {i: get_emulator_url(i, emulator_base_url) for i in self.loaded_idxs}
 
         self.pre_initialize()
